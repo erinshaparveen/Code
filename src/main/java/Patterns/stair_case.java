890e3de3ceffa0bc07e617e9b1b4105fe1561794
INSHA <er.inshaParveen@gmail.com>
2025-05-28 23:35:18 +0530

    square_Rectangle Pattern Stairs Patter

---
+++ src/main/java/Patterns/stair_case.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class stair_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<n-i;j++)  // to print the space
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++)  // to print the pattern
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
