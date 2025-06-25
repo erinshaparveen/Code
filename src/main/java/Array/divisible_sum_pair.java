194d2c125ecfc3652183a20e39e3216cbc74bf9d
INSHA <er.inshaParveen@gmail.com>
2025-06-25 23:02:17 +0530

    1D array

---
+++ src/main/java/Array/divisible_sum_pair.java
@@ -0,0 +1,52 @@
+package Array;
+import java.util.Scanner;
+//Divisible Sum Pairs
+//Given an array of integers, arr, and a positive integer k, determine the number of (i,j) pairs in the array where i<j and arr[i] + arr[j] is divisible by k.
+//
+//Input Format
+//Input consists of two lines.
+//
+//The first line contains 2 space-separated integers, n (size of array) and k.
+//
+//The second line contains n space-separated integers, each a value of arr[i].
+//
+//Output Format
+//Print the number of pairs a[i] + a[j] divisble by k, where i<j.
+//
+//Example 1
+//Input
+//
+//6 3
+//        1 3 2 6 1 2
+//Output
+//
+//5
+public class divisible_sum_pair {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int target_value = sc.nextInt();
+        int[] arr = new int[length];
+        for(int i=0;i<length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(divisible_sum_pair_count(arr, length, target_value));
+    }
+
+    public static int divisible_sum_pair_count(int[] arr, int arr_length, int target_value)
+    {
+        int count = 0;
+        for(int i = 0; i<arr_length; i++)
+        {
+            for(int j = i+1; j<arr_length;j++)
+            {
+                if((arr[i]+arr[j])%target_value == 0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/Array/geometric_tripler.java
@@ -0,0 +1,83 @@
+package Array;
+
+import java.util.Scanner;
+
+//Find Geometric Triplets
+//Write a program to print all the triplets that form a geometric progression. All the elements of the array are in increasing order of their value.
+//
+//        Input Format
+//Input consists of two lines.
+//
+//First line contains an integer N denoting size of array
+//
+//Second line contains N integers denoting the array elements separated by single space
+//
+//Output Format
+//Print all the triplets that form a geometric progression, each in a new line. Print each geometric triplet in lexicographic form i.e. if a set of triplet has a lesser value at the same position compared to some other triplet then it has to be printed before the latter one.
+//
+//        Example 1
+//Input
+//
+//6
+//        1 2 6 10 18 54
+//Output
+//
+//2 6 18
+//        6 18 54
+//Example 2
+//Input
+//
+//8
+//        2 8 10 15 16 30 32 64
+//Output
+//
+//        2 8 32
+//        8 16 32
+//        16 32 64
+public class geometric_tripler {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+        for(int i=0;i<length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        geometric_tripler(arr, length);
+    }
+
+    public static void geometric_tripler(int[] arr, int length)
+    {
+        for(int i = 0 ; i < length ; i++)
+        {
+            int first = arr[i];
+            int second = first*3;
+            int third = second *3;
+            boolean found_second = false;
+            boolean found_third = false;
+            for(int j = i+1 ; j < length ; j++)
+            {
+                if(arr[j] == second)
+                {
+                    found_second = true;
+                    break;
+                }
+            }
+            if(found_second)
+            {
+                for(int j = i+1 ; j < length ; j++)
+                {
+                    if(arr[j] == third)
+                    {
+                        found_third = true;
+                        break;
+                    }
+                }
+            }
+            if(found_second && found_third)
+            {
+                System.out.println(first+" "+second+" "+third);
+            }
+        }
+    }
+}
