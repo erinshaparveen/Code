b927951b032250a9980579fb8ce1735e4461df39
INSHA <er.inshaParveen@gmail.com>
2026-01-07 13:14:21 +0530

    interview january

--- src/main/java/Revesion/evenOdd.java
+++ src/main/java/Revesion/evenOdd.java
@@ -6,7 +6,9 @@ public class evenOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        if(number%2==0) System.out.println("Even");
-        else System.out.println("Odd");
+        if(number%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
     }
 }
